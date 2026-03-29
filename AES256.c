fc392608811e17e942ce0496b726220a3252783a
SATURNXVZ <vvitorhenriique@gmail.com>
2026-03-29 09:48:11 -0300

    Update Main (AES 1.2)

--- AES256.c
+++ AES256.c
@@ -2,14 +2,177 @@
 #include <stdio.h>
 #include <stdint.h> 
 #include <string.h>
+#include <memory.h>
+
 
 #define msg 40 //tamanho máximo do texto
 #define buffer (msg + 16) //tamanho máximo de capacidade de espaço (para padding)
+
 
 // AES-256 constantes
 #define AES_KEY_SIZE 32
 #define AES_ROUNDS 14
 #define AES_ROUND_KEYS 15
+
+
+//inicio sha 256
+#define ROTLEFT(a,b) (((a) << (b)) | ((a) >> (32-(b))))
+#define ROTRIGHT(a,b) (((a) >> (b)) | ((a) << (32-(b))))
+
+#define CH(x,y,z) (((x) & (y)) ^ (~(x) & (z)))
+#define MAJ(x,y,z) (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
+#define EP0(x) (ROTRIGHT(x,2) ^ ROTRIGHT(x,13) ^ ROTRIGHT(x,22))
+#define EP1(x) (ROTRIGHT(x,6) ^ ROTRIGHT(x,11) ^ ROTRIGHT(x,25))
+#define SIG0(x) (ROTRIGHT(x,7) ^ ROTRIGHT(x,18) ^ ((x) >> 3))
+#define SIG1(x) (ROTRIGHT(x,17) ^ ROTRIGHT(x,19) ^ ((x) >> 10))
+
+
+//typedef para sha256
+typedef uint8_t BYTE;
+typedef uint32_t DWORD;
+typedef uint64_t ULONGLONG;
+typedef uint32_t WORD;
+
+
+//estrutura para sha256
+typedef struct {
+    BYTE data[64];
+    DWORD datalen;
+    ULONGLONG bitlen;
+    DWORD state[8];
+} SHA256_CTX;
+
+
+//sha256
+static const WORD k[64] = {
+	0x428a2f98,0x71374491,0xb5c0fbcf,0xe9b5dba5,0x3956c25b,0x59f111f1,0x923f82a4,0xab1c5ed5,
+	0xd807aa98,0x12835b01,0x243185be,0x550c7dc3,0x72be5d74,0x80deb1fe,0x9bdc06a7,0xc19bf174,
+	0xe49b69c1,0xefbe4786,0x0fc19dc6,0x240ca1cc,0x2de92c6f,0x4a7484aa,0x5cb0a9dc,0x76f988da,
+	0x983e5152,0xa831c66d,0xb00327c8,0xbf597fc7,0xc6e00bf3,0xd5a79147,0x06ca6351,0x14292967,
+	0x27b70a85,0x2e1b2138,0x4d2c6dfc,0x53380d13,0x650a7354,0x766a0abb,0x81c2c92e,0x92722c85,
+	0xa2bfe8a1,0xa81a664b,0xc24b8b70,0xc76c51a3,0xd192e819,0xd6990624,0xf40e3585,0x106aa070,
+	0x19a4c116,0x1e376c08,0x2748774c,0x34b0bcb5,0x391c0cb3,0x4ed8aa4a,0x5b9cca4f,0x682e6ff3,
+	0x748f82ee,0x78a5636f,0x84c87814,0x8cc70208,0x90befffa,0xa4506ceb,0xbef9a3f7,0xc67178f2
+};
+
+
+
+void sha256_transform(SHA256_CTX *ctx, const BYTE data[])
+{
+	WORD a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
+
+	for (i = 0, j = 0; i < 16; ++i, j += 4)
+		m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
+	for ( ; i < 64; ++i)
+		m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
+
+	a = ctx->state[0];
+	b = ctx->state[1];
+	c = ctx->state[2];
+	d = ctx->state[3];
+	e = ctx->state[4];
+	f = ctx->state[5];
+	g = ctx->state[6];
+	h = ctx->state[7];
+
+	for (i = 0; i < 64; ++i) {
+		t1 = h + EP1(e) + CH(e,f,g) + k[i] + m[i];
+		t2 = EP0(a) + MAJ(a,b,c);
+		h = g;
+		g = f;
+		f = e;
+		e = d + t1;
+		d = c;
+		c = b;
+		b = a;
+		a = t1 + t2;
+	}
+
+	ctx->state[0] += a;
+	ctx->state[1] += b;
+	ctx->state[2] += c;
+	ctx->state[3] += d;
+	ctx->state[4] += e;
+	ctx->state[5] += f;
+	ctx->state[6] += g;
+	ctx->state[7] += h;
+}
+
+void sha256_init(SHA256_CTX *ctx)
+{
+	ctx->datalen = 0;
+	ctx->bitlen = 0;
+	ctx->state[0] = 0x6a09e667;
+	ctx->state[1] = 0xbb67ae85;
+	ctx->state[2] = 0x3c6ef372;
+	ctx->state[3] = 0xa54ff53a;
+	ctx->state[4] = 0x510e527f;
+	ctx->state[5] = 0x9b05688c;
+	ctx->state[6] = 0x1f83d9ab;
+	ctx->state[7] = 0x5be0cd19;
+}
+
+void sha256_update(SHA256_CTX *ctx, const BYTE data[], size_t len)
+{
+	WORD i;
+
+	for (i = 0; i < len; ++i) {
+		ctx->data[ctx->datalen] = data[i];
+		ctx->datalen++;
+		if (ctx->datalen == 64) {
+			sha256_transform(ctx, ctx->data);
+			ctx->bitlen += 512;
+			ctx->datalen = 0;
+		}
+	}
+}
+
+void sha256_final(SHA256_CTX *ctx, BYTE hash[])
+{
+	WORD i;
+
+	i = ctx->datalen;
+
+	// Pad whatever data is left in the buffer.
+	if (ctx->datalen < 56) {
+		ctx->data[i++] = 0x80;
+		while (i < 56)
+			ctx->data[i++] = 0x00;
+	}
+	else {
+		ctx->data[i++] = 0x80;
+		while (i < 64)
+			ctx->data[i++] = 0x00;
+		sha256_transform(ctx, ctx->data);
+		memset(ctx->data, 0, 56);
+	}
+
+	// Append to the padding the total message's length in bits and transform.
+	ctx->bitlen += ctx->datalen * 8;
+	ctx->data[63] = ctx->bitlen;
+	ctx->data[62] = ctx->bitlen >> 8;
+	ctx->data[61] = ctx->bitlen >> 16;
+	ctx->data[60] = ctx->bitlen >> 24;
+	ctx->data[59] = ctx->bitlen >> 32;
+	ctx->data[58] = ctx->bitlen >> 40;
+	ctx->data[57] = ctx->bitlen >> 48;
+	ctx->data[56] = ctx->bitlen >> 56;
+	sha256_transform(ctx, ctx->data);
+
+	// Since this implementation uses little endian byte ordering and SHA uses big endian,
+	// reverse all the bytes when copying the final state to the output hash.
+	for (i = 0; i < 4; ++i) {
+		hash[i]      = (ctx->state[0] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 4]  = (ctx->state[1] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 8]  = (ctx->state[2] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 12] = (ctx->state[3] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 16] = (ctx->state[4] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 20] = (ctx->state[5] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 24] = (ctx->state[6] >> (24 - i * 8)) & 0x000000ff;
+		hash[i + 28] = (ctx->state[7] >> (24 - i * 8)) & 0x000000ff;
+	}
+}
+//FIM DO SHA256
 
 
 // S-BOX - Tabela de substituição do AES
@@ -202,8 +365,8 @@ void mixColumns(uint8_t estado[4][4]){
     }
 }
 
-void keyDerive(){
-
+void keyDerive(const char *senha, uint8_t *chave) {
+    sha256((const uint8_t*)senha, strlen(senha), chave);
 }
 
 // Faz XOR do estado com a chave da rodad
@@ -309,12 +472,23 @@ int invPadding(uint8_t *cripto, int tam){
    }
 
 
-   for(int i = 0; i > tam; i++){
-        if(cripto[tam - i - value] != value){
+   for(int i = 0; i < value; i++){
+        if(cripto[tam - 1 - i] != value){
             printf("\nERRO! Ultimo byte diferente!\n");
+            return -1;
         }
    }
+
+   return tam - value;
 }
+
+void sha256(const uint8_t *data, size_t len, uint8_t *hash) {
+    SHA256_CTX ctx;
+    sha256_init(&ctx);
+    sha256_update(&ctx, data, len);
+    sha256_final(&ctx, hash);
+}
+
 
 //funcao principal da descripto (AES-256)
 void invAES(const uint8_t *cripto, const uint8_t roundKeys[AES_ROUND_KEYS][16], uint8_t *text) {
@@ -387,53 +561,29 @@ void AES(const uint8_t *text, const uint8_t roundKeys[AES_ROUND_KEYS][16], uint8
 }
 
 int main() {
-    printf("=== TESTE AES-256 ===\n\n");
+    char senha[100];
+
+    //1- senha usuario
+    printf("Digite uma senha: ");
+    fgets(senha, sizeof(senha), stdin);
+    senha[strcspn(senha, "\n")] = 0;
+    printf("\n%s", senha);
+
+    //2- Derivar chave
+    uint8_t chave[32];
+    keyDerive(senha, chave);
     
-    // Chave de teste (32 bytes) para AES-256
-    uint8_t chave[32] = {
-        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
-        0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
-        0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
-        0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f
-    };
-    
-    // Texto claro (16 bytes)
-    uint8_t text_original[16] = {
-        0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
-        0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff
-    };
-    
-    uint8_t roundKeys[AES_ROUND_KEYS][16];
-    uint8_t cripto[16];
-    uint8_t text[16];
-    
-    // Expande a chave
-    keyExpansion(chave, roundKeys);
-    
-    printf("Chave (32 bytes):\n");
+    printf("Chave derivada (32 bytes): \n");
     printHex(chave, 32);
-    
-    printf("\nTexto original:\n");
-    printHex(text_original, 16);
-    
-    // CRIPTOGRAFAR
-    AES(text_original, roundKeys, cripto);
-    
-    printf("\nTexto cifrado:\n");
-    printHex(cripto, 16);
-    
-    // DESCRIPTOGRAFAR
-    invAES(cripto, roundKeys, text);
-    
-    printf("\nTexto decifrado:\n");
-    printHex(text, 16);
-    
-    // Verifica se voltou ao original
-    if(memcmp(text_original, text, 16) == 0) {
-        printf("\n✅ DESCRIPTOGRAFIA FUNCIONOU! AES-256 CORRETO!\n");
-    } else {
-        printf("\nDESCRIPTOGRAFIA FALHOU\n");
-    }
-    
-    return 0;
+
+    //3- Expande chave
+    uint8_t roundKeys[AES_ROUND_KEYS][16];
+    keyExpansion(chave, roundKeys);
+
+
+    //4- Mensagem do usuario
+    uint8_t mensagem[buffer];
+    printf("\n\nDigite a mensagem: ");
+    fgets((char*)mensagem [strcspn((char*) mensagem, "\n")] = 0; 
+
 }
